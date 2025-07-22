74e45276b50253cef6d086ab287c24d1f176129c
vansh <vansh55dec@gmail.com>
2025-07-22 22:49:02 +0530

    routs 220725

--- src/app.js
+++ src/app.js
@@ -1,9 +1,30 @@
 const express = require("express");
 
 const app = express();
-console.log(app.toString());
-app.use("/test",(req,res)=>{
-    res.send("hellow")
+// console.log(app.toString());
+
+app.use((req,res,next)=>{
+    next( new Error("errrrroooooooooooor"));
+})
+
+app.use("/admin",(req,res,next)=>{
+    console.log("admin auth")
+    let name = "o";
+    if(name === "o") next();
+    else res.status(401).send("no access"); 
+    // res.send("hi");
+})
+
+app.get("/admin/getdata",(req,res)=>{
+    res.send("gettingData");
+})
+
+app.get("/admin/deletedata",(req,res)=>{
+    res.send("deleting data");
+})
+
+app.use((err,req,res,next)=>{
+    res.status(500).send("error");
 })
 
 app.listen(9000,()=>{
